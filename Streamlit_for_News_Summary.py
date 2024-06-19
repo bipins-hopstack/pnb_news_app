604c3df718532eb87f0654c9aa1945e77f675e6d
bipins-hopstack <Bipinsreddy@gmail.com>
2024-06-19 17:40:40 +0530

    Update Streamlit_for_News_Summary.py

--- Streamlit_for_News_Summary.py
+++ Streamlit_for_News_Summary.py
@@ -20,7 +20,8 @@ df1 = pd.read_csv(url1)
 # In[94]:
 
 url2='https://github.com/bipins-hopstack/pnb_news_app/blob/main/SEBI_PFRDA.xlsx?raw=true'
-df2 = pd.read_excel(url2)
+myfile = requests.get(url2)
+df2 = pd.read_excel(myfile.content)
 
 
 # In[95]:
