be775749f02a5329b3750916d5737d8564711eeb
bipins-hopstack <Bipinsreddy@gmail.com>
2024-06-21 15:35:14 +0530

    updated news till 20-06-2024

--- Streamlit_for_News_Summary.py
+++ Streamlit_for_News_Summary.py
@@ -1,59 +1,52 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[92]:
+# In[41]:
 
 
 import streamlit as st
 import pandas as pd
 from datetime import datetime, timedelta
 from PIL import Image
-import requests
-from io import BytesIO
-
-url = 'https://github.com/bipins-hopstack/pnb_news_app/blob/main/PNBLogo.png?raw=true'
-response = requests.get(url)
-#img = Image.open(BytesIO(response.content))
-
-# In[93]:
-
-url1 = 'https://github.com/bipins-hopstack/pnb_news_app/blob/main/RBI_18JUN.csv?raw=true'
-df1 = pd.read_csv(url1)
 
 
-
-# In[94]:
-
-url2='https://github.com/bipins-hopstack/pnb_news_app/blob/main/SEBI_PFRDA.csv?raw=true'
-
-df2 = pd.read_csv(url2)
+# In[42]:
 
 
-# In[95]:
+df1 = pd.read_excel(r'D:\Shekhar\Daily\June2024\21.06.2024\RBI_21JUN.xlsx')
 
 
-consolidated_1 = '''\n- RBI will conduct a Variable Rate Repo (VRR) auction on June 18, 2024, with the same guidelines as outlined in Press Release 2021-2022/1572 dated January 20, 2022.\n- Purvanchal Co-operative Bank in Uttar Pradesh has had its license cancelled by RBI due to insufficient capital and poor earning prospects. Depositors are entitled to insurance claim amounts up to ₹5 lakh from the Deposit Insurance and Credit Guarantee Corporation.\n- Premature redemption of Sovereign Gold Bonds issued in December 2017 will be allowed from June 18, 2024, after five years from the date of issue. The redemption price will be based on the simple average of the closing gold price of the previous three business days before the date of redemption.\n- RBI will sell two dated securities worth ₹23,000 crore through auction using the multiple price method. Both competitive and non-competitive bids are accepted, and interest on the securities will generally be paid half-yearly.\n- RBI has fined Sonali Bank PLC ₹96.40 lakh for violating the Credit Information Companies (Regulation) Act, 2005 and not complying with the Know Your Customer (KYC) directions and SWIFT-related operational controls.\n- RBI has fined Central Bank of India ₹1.45 crore for violating regulatory directions related to loans and customer protection.'''
+# In[43]:
 
 
-# In[96]:
+df2 = pd.read_excel(r'D:\Shekhar\Daily\June2024\21.06.2024\SEBI_PFRDA_21JUN.xlsx')
 
 
-consolidated_2 = '''\n- SEBI has launched a free, voluntary online certification program in collaboration with NISM to help investors test their knowledge of markets and investing. The certification aims to assist individuals in gaining comprehensive knowledge about investing in the Indian securities markets.\n- SEBI has updated the "Saaṛthi2.0" mobile app, featuring a user-friendly interface, financial calculators, modules on KYC procedures, mutual funds, buying and selling shares, investor grievances redressal mechanism, and the Online Dispute Resolution (ODR) platform.\n- IRDAI has simplified the regulatory framework for the insurance sector, consolidating 37 regulations into 7 and introducing two new regulations, effective from April 1, 2024. IRDAI has also issued a Master Circular on Life Insurance business, consolidating provisions from four existing circulars, introducing a Customer Information Sheet, mandatory policy loans, health riders, partial withdrawals, and a longer free look period. The move aims to simplify and enhance transparency in life insurance, making it easier for policyholders to understand and make informed decisions'''
+# In[44]:
 
 
-# In[97]:
+df3 = pd.read_excel(r'D:\Shekhar\Daily\June2024\21.06.2024\PIB_21JUN.xlsx')
 
 
-# To get one day ago details
-
-# current_datetime = datetime.now() - timedelta(days=1)
-# formatted_datetime = current_datetime.strftime("%Y-%m-%d %H:%M:%S")
-
-# Get current date and time
-#current_datetime = datetime.now().strftime("%Y-%m-%d")
+# In[45]:
 
 
-# In[98]:
+rbi_gist = df1.iloc[0]['Gist']
+
+
+# In[46]:
+
+
+sebi_gist = df2.iloc[0]['Gist']
+
+
+# In[47]:
+
+
+pib_gist = df3.iloc[0]['Gist']
+
+
+# In[48]:
 
 
 def add_logo(logo_path, width, height):
@@ -63,7 +56,7 @@ def add_logo(logo_path, width, height):
     return modified_logo
 
 
-# In[99]:
+# In[49]:
 
 
 # Function to display dataframe with text wrap and hyperlinks
@@ -73,21 +66,20 @@ def display_dataframe(df):
         st.markdown(f"[Click Here to access News URL]({row['Link']})")
 
 
-# In[100]:
+# In[50]:
 
 
 # Streamlit UI
 st.title("News Dashboard")
-st.header('14th June to 18th June 2024')
 
 # Sidebar with collapsible section for RBI News
-# st.sidebar.image(add_logo(logo_path=img, width=175, height=85), use_column_width=True)
-st.sidebar.markdown('## RBI SEBI IRDAI')
+st.sidebar.image(add_logo(logo_path="PNBLogo.png", width=175, height=85), use_column_width=True)
+st.sidebar.markdown('## NEWS - RBI/SEBI/IRDI/PIB <span style="font-size: medium">(14th June to 18th June 2024)</span>', unsafe_allow_html=True)
 
 # Radio button to select the news category
 news_category = st.sidebar.radio(
     "Select News Category",
-    ('RBI News', 'SEBI & IRDAI News')
+    ('RBI News', 'SEBI & IRDAI News','PIB News')
 )
 
 # Options for displaying news
@@ -102,12 +94,17 @@ elif news_category == 'SEBI & IRDAI News':
         "Select News Option",
         ('Gist of the News', 'News Headings with Summary', 'News Headings with URLs')
     )
+elif news_category == 'PIB News':
+    news_option = st.sidebar.radio(
+        "Select News Option",
+        ('Gist of the News', 'News Headings with Summary', 'News Headings with URLs')
+    )
 
 # Display selected news based on category and option
 if news_category == 'RBI News':
     if news_option == 'Gist of the News':
         st.header("Gist of the News")
-        st.write(consolidated_1)
+        st.write(rbi_gist)
     elif news_option == 'News Headings with Summary':
         st.header("News Headings with Summary")
         for heading,summary in zip(df1['Headings'],df1['Summary']):
@@ -120,7 +117,7 @@ if news_category == 'RBI News':
 elif news_category == 'SEBI & IRDAI News':
     if news_option == 'Gist of the News':
         st.header("Gist of the News")
-        st.write(consolidated_2)
+        st.write(sebi_gist)
     elif news_option == 'News Headings with Summary':
         st.header("News Headings with Summary")
         for heading,summary in zip(df2['Headings'],df2['Summary']):
@@ -129,6 +126,19 @@ elif news_category == 'SEBI & IRDAI News':
     elif news_option == 'News Headings with URLs':
         st.header("News Headings with URLs")
         display_dataframe(df2)
+        
+elif news_category == 'PIB News':
+    if news_option == 'Gist of the News':
+        st.header("Gist of the News")
+        st.write(pib_gist)
+    elif news_option == 'News Headings with Summary':
+        st.header("News Headings with Summary")
+        for heading,summary in zip(df3['Headings'],df3['Summary']):
+            st.markdown(f"•  **{heading}**")
+            st.write(summary)
+    elif news_option == 'News Headings with URLs':
+        st.header("News Headings with URLs")
+        display_dataframe(df3)
 
 
 # In[ ]:
