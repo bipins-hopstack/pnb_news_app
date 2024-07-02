5ce215c6652a2de2b3e1dcec24b1d3b9008dd308
AIwithSS <160053160+AIwithSS@users.noreply.github.com>
2024-07-02 11:07:56 +0530

    Update newsappv03.py

--- newsappv03.py
+++ newsappv03.py
@@ -182,7 +182,7 @@ st.title("News Dashboard")
 
 # Sidebar with collapsible section for RBI News
 st.sidebar.image(add_logo(logo_path="PNBLogo.png", width=175, height=85), use_column_width=True)
-st.sidebar.markdown('## NEWS - RBI/SEBI/IRDAI/PIB <span style="font-size: medium">28th June 2024</span>', unsafe_allow_html=True)
+st.sidebar.markdown('## NEWS - RBI/SEBI/IRDAI/PIB <span style="font-size: medium">01st July 2024</span>', unsafe_allow_html=True)
 
 # Radio button to select the news category
 news_category = st.sidebar.radio(
